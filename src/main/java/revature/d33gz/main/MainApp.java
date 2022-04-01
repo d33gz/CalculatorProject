93d3786269a9c73a8e362d9804585c7f0abe66e0
Hendo1349 <hendo1349@gmail.com>
2022-04-01 15:04:13 -0400

    initial commit

---
+++ src/main/java/revature/d33gz/main/MainApp.java
@@ -0,0 +1,8 @@
+package revature.d33gz.main;
+
+public class MainApp {
+	public static void main(String[] args) {
+		Calculator c=new Calculator();
+		System.out.println("Adding Two Numbers : " +c.add(2, 2));
+	}
+}
