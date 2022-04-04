83974bc4e943f052ea8eeb79172095a0c0f89e99
Hendo1349 <hendo1349@gmail.com>
2022-04-04 12:02:51 -0400

    added subtract syso

--- src/main/java/revature/d33gz/main/MainApp.java
+++ src/main/java/revature/d33gz/main/MainApp.java
@@ -4,5 +4,6 @@ public class MainApp {
 	public static void main(String[] args) {
 		Calculator c = new Calculator();
 		System.out.println("Adding Two Numbers : " + c.add(2, 2));
+		System.out.println("Subtracting Two Numbers: " + c.subtract(4, 2));
 	}
 }
