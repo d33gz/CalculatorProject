5aa6a35f5547a715c35edae376aaee0c4f0a7f1a
Hendo1349 <hendo1349@gmail.com>
2022-04-04 11:32:58 -0400

    package test run

--- src/main/java/revature/d33gz/main/MainApp.java
+++ src/main/java/revature/d33gz/main/MainApp.java
@@ -2,7 +2,7 @@ package revature.d33gz.main;
 
 public class MainApp {
 	public static void main(String[] args) {
-		Calculator c=new Calculator();
-		System.out.println("Adding Two Numbers : " +c.add(2, 2));
+		Calculator c = new Calculator();
+		System.out.println("Adding Two Numbers : " + c.add(2, 2));
 	}
 }
